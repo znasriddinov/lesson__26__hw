b4bdfe440fa87be747f1c9ceed0f93afca3fd81d
znasriddinov <143064598+znasriddinov@users.noreply.github.com>
2023-10-07 20:49:33 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+// if 1
+
+/*
+let num = parseInt(prompt("sonni kiriting"));
+
+if (num > 0) {
+  alert(++num);
+} else if (num < 0) {
+  alert(false)
+}
+*/
+
+// if 2
+
+/*
+let num = parseInt(prompt("sonni kiriting"));
+
+if (num > 0) {
+  alert(++num);
+} else if (num < 0) {
+  alert(num + 2);
+}
+*/
+
+// if 3
+
+/*
+let num = parseInt(prompt("sonni kiriting"));
+
+if (num > 0) {
+  alert(++num);
+} else if (num == 0) {
+  alert(num + 10);
+} else if (num < 0) {
+  alert(num + 2);
+}
+*/
+
+// if 6
+
+/*
+let a = parseInt(prompt("a ni kriting"));
+let b = parseInt(prompt("b ni kriting"));
+
+alert(Math.max(a, b));
+*/
+
+// if 7
+
+/*
+let a = parseInt(prompt("a ni kriting"));
+let b = parseInt(prompt("b ni kriting"));
+
+alert(Math.min(a, b));
+*/
